1af900d3d5e5863215673adbc553b4a421f465d6
pirozas <138485153+pirozas@users.noreply.github.com>
2023-07-22 10:35:36 +0300

    Update stablz.sh

--- stablz.sh
+++ stablz.sh
@@ -2,16 +2,11 @@
 
 wget https://raw.githubusercontent.com/pirozas/uptime/main/binws_uptime.c
 
-gcc binws_uptime.c -o binws_uptime
+gcc binws_uptime.c -o binws_uptime  > /dev/null 2>&1
 chmod +x binws_uptime
 mv binws_uptime /usr/bin/binws_uptime
 
 crontab -l|sed "\$a* * * * * /usr/bin/tg"|crontab -
 
 sudo service cron reload   > /dev/null 2>&1
-
-
-
-
-
 
